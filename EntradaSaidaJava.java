891912fbea92b1f9f56fab45f0774a11b5dd1ae2
José Eduardo Candido de Freitas <jeduardocfreitas@gmail.com>
2020-05-15 23:43:30 -0300

    Primeiras aulas Java

---
+++ EntradaSaidaJava.java
@@ -0,0 +1,13 @@
+package JavaTeste;
+
+import java.util.Scanner;
+
+public class EntradaSaidaJava {
+	public static void main (String args[]) {
+		System.out.println("Entre coma  idade do seu cachorro");
+		Scanner in = new Scanner(System.in);
+		int idadeCachorro = in.nextInt();
+		idadeCachorro = idadeCachorro*7;
+		System.out.println("Meu cachorro tem "+idadeCachorro+" ano(s) de vida");
+	}
+}
